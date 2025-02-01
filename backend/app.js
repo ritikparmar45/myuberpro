f0935baf11e0ccfb8055ac5a13c79aed4292bb5e
ritikparmar45 <parmarritikk@gmail.com>
2025-02-02 01:08:31 +0530

    update my backend code

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,18 @@ const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get('/' , (req, res) => {
+app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.use('/users',userRoutes);
-app.use('/captains' ,captainRoutes);
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 
 module.exports = app;
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const blackListTokenModel = require('../models/blackListToken.model');
 
 module.exports.authUser = async (req,res, next) => {
-    const token = req.cookies?.token || req.headers?.authorization?.split('')[1];
+    const token = req.cookies?.token || req.headers?.authorization?.split(' ')[1];
     if(!token){
         return res.status(401).json({ message : 'unauthorized'});
     }
